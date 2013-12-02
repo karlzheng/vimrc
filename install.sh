fd76ade1f84e1f93835c27dea592c6c234318774
kangliang.zkl <kangliang.zkl@alibaba-inc.com>
2013-12-02 20:07:04 +0800

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,30 @@
-#!/bin/bash
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE:  install.sh
+#
+#         USAGE:  ./install.sh
+#
+#   DESCRIPTION:
+#
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR: Karl Zheng (), ZhengKarl#gmail.com
+#       COMPANY: Alibaba
+#       CREATED: 2013年12月02日 20时03分52秒 CST
+#      REVISION:  ---
+#===============================================================================
 
-if [ -f ${HOME}/.vimrc ];then
-    ln -s ${HOME}/.vim/.vimrc ${HOME}/.vimrc 
+set -o nounset                              # Treat unset variables as an error
+
+
+
+if [ ! -f ${HOME}/.vimrc ];then
+    ln -s $(pwd)/.vimrc ${HOME}/.vimrc
+fi
+
+if [ ! -d ${HOME}/.vim ];then
+    ln -s $(pwd)/.vim ${HOME}/.vim
 fi
